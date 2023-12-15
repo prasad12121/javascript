c1851ad0c5a478cfb35241a13054567b26a410b2
prasadpalase <59653289+prasad12121@users.noreply.github.com>
2023-12-15 18:01:48 +0530

    Add files via upload

---
+++ b1.js
@@ -0,0 +1,16 @@
+
+function add(a,b){
+    return a+b;
+}
+
+function devide(a,b){
+    return a/b;
+}dfdfdf
+dfdfdf
+ghghgh
+function calculate(x,y,operation){
+    return x,y;
+}
+
+
+console.log(calculate(2,3,add));
